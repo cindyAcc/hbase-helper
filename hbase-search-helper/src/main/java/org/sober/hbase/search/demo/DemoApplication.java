3c0a8c221eeb10a1fbb6f251b2c25c66caed5469
liang.li <liang.li@tongfudun.com>
2024-03-05 13:32:05 +0800

    optimize - demo

--- hbase-search-helper/src/main/java/org/sober/hbase/search/demo/DemoApplication.java
+++ hbase-search-helper/src/main/java/org/sober/hbase/search/demo/DemoApplication.java
@@ -1,9 +1,15 @@
 package org.sober.hbase.search.demo;
 
 import org.sober.hbase.search.config.EnableHbaseRepositoryAutoConfig;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableHbaseRepositoryAutoConfig
 public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
 }
